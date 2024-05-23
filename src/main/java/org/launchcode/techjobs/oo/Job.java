19ba2c327972d71334a6306230247dd8076770d9
fowlerrc <142541725+fowlerrc@users.noreply.github.com>
2024-05-23 18:57:38 -0500

    Assignment 2

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -13,7 +13,111 @@ public class Job {
     private PositionType positionType;
     private CoreCompetency coreCompetency;
 
-    // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
+
+    public Job () {
+        id = nextId;
+        nextId++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this.id = nextId++; //calling the first constructor to initialize the id field
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
+    @Override
+    public String toString() {
+
+        if (name.isEmpty()) {
+            name = ("Data not available");
+        }
+        if (employer.getValue().equals("") || employer.getValue() == null) {
+            employer.setValue("Data not available");
+
+        }
+
+
+        if (location.getValue().equals("") || location.getValue() == null) {
+            location.setValue("Data not available");
+
+        } if (positionType.getValue().equals("") || positionType.getValue() == null) {
+           positionType.setValue("Data not available");
+
+        } if (coreCompetency.getValue().equals("") || coreCompetency.getValue() == null) {
+            coreCompetency.setValue("Data not available");
+        }
+
+
+        return System.lineSeparator() + "ID: " + this.id + System.lineSeparator() +
+                "Name: " + this.name + System.lineSeparator() +
+                "Employer: " + this.employer + System.lineSeparator() +
+                "Location: " + this.location + System.lineSeparator() +
+                "Position Type: " + this.positionType + System.lineSeparator() +
+                "Core Competency: " + this.coreCompetency + System.lineSeparator();
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job job)) return false;
+        return getId() == job.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,78 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertTrue(job.getEmployer() instanceof Employer);
+        assertEquals("ACME", job.getEmployer().getValue());
+
+        assertTrue(job.getLocation() instanceof Location);
+        assertEquals("Desert", job.getLocation().getValue());
+
+        assertTrue(job.getPositionType() instanceof PositionType);
+        assertEquals("Quality control", job.getPositionType().getValue());
+
+        assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Persistence", job.getCoreCompetency().getValue());
+
+        assertTrue(job instanceof Job);
+        assertEquals("Product tester", job.getName());
+
+    }
+
+    @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+
+
+        assertTrue(job.toString().endsWith(System.lineSeparator()));
+        assertTrue(job.toString().startsWith(System.lineSeparator()));
+
+
+    }
+
+    @Test
+    public void testToStringContainsCorrectLabelsAndData() {
+        //Job job = new Job("Product tester", new Employer("ACME"), new Location());
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+
+
+        String expected = System.lineSeparator() + "ID: " + job.getId() + System.lineSeparator() +
+                "Name: " + job.getName() + System.lineSeparator() +
+                "Employer: " + job.getEmployer() + System.lineSeparator() +
+                "Location: " + job.getLocation() + System.lineSeparator() +
+                "Position Type: " + job.getPositionType() + System.lineSeparator() +
+                "Core Competency: " + job.getCoreCompetency() + System.lineSeparator();
+
+        String actual = job.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testToStringHandlesEmptyField() {
+        Job job = new Job("", new Employer(""), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        String expected = System.lineSeparator() + "ID: " + job.getId() + System.lineSeparator() +
+                "Name: Data not available" + System.lineSeparator() +
+                "Employer: Data not available" + System.lineSeparator() +
+                "Location: Desert" + System.lineSeparator() +
+                "Position Type: Quality control" + System.lineSeparator() +
+                "Core Competency: Persistence" + System.lineSeparator();
+
+        String actual = job.toString();
+
+        assertEquals(expected, actual);
+    }
 }
+
